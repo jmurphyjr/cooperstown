342d1b035c8da1a39cd97b7e3d0f203c07096456
Jack Murphy <jmurphy.jr@gmail.com>
2016-03-07 22:57:28 -0500

    Initial dist subtree commit

---
+++ js/perfmatters.js
@@ -0,0 +1,18 @@
+// Measuring the Critical Rendering Path with Navigation Timing
+// https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
+/* jshint node: true */
+/* global window */
+'use strict';
+
+function logCRP() {
+    var t = window.performance.timing;
+    var dcl = t.domContentLoadedEventStart - t.domLoading;
+    var complete = t.domComplete - t.domLoading;
+    var stats = document.getElementById('crp-stats');
+    stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
+    console.log('DCL: ' + dcl + 'ms, onload: ' + complete + 'ms');
+}
+
+window.addEventListener('load', function () {
+    logCRP();
+});
